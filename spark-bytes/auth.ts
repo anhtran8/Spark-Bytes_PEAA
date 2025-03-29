4856fcea808caedd3c4f69aae624e3a57a215e71
Akemi Sai <126123381+achemai@users.noreply.github.com>
2025-03-29 14:49:07 -0400

    added supabase backend stuff, now when user login with buid, it creates a user record for that user(new file, lib/supabase.ts, changes in auth.ts, .env.local)

--- spark-bytes/auth.ts
+++ spark-bytes/auth.ts
@@ -1,6 +1,7 @@
 // auth.ts
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { supabase } from "./lib/supabase"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
@@ -9,5 +10,26 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { data, error } = await supabase
+        .from("users")
+        .upsert([
+          { 
+            id: user.id, 
+            name: user.name, 
+            email: user.email,
+            dietary_preferences: [],
+          }], {
+          onConflict: "id",
+        });
+
+        if (error) {
+          console.error("Error upserting user:", error);
+          return false;
+        }
+        return true;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 })
